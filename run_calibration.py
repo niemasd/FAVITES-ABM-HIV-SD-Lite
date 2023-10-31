54943fc480470af99d72f711850682b7586d7d3b
Niema Moshiri <niemamoshiri@gmail.com>
2023-10-31 15:38:15 -0700

    Updated calibration code

--- run_calibration.py
+++ run_calibration.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-
-# TODO REWRITE TO CALL EXTERNAL "SIMULATION FOLDER SCORING" HELPER SCRIPT INTEAD OF SCORING INTERNALLY
-
 from run_favites_lite import DEFAULT_PATH_ABM_HIV_COMMANDLINE, DEFAULT_PATH_ABM_HIV_MODULES, DEFAULT_PATH_COATRAN_CONSTANT
 from csv import reader
 from datetime import datetime
+from glob import glob
 from openpyxl import load_workbook
-from os import chdir, getcwd, makedirs
+from os import chdir, cpu_count, getcwd, makedirs
 from os.path import abspath, expanduser, isdir, isfile
 from random import randint
 from scipy.optimize import minimize
+from statistics import mean
 from subprocess import check_output
 from sys import argv, stdout
 import argparse
@@ -49,6 +48,8 @@ def parse_args():
     parser.add_argument('--time_tree_only_include_mapped', action='store_true', help="Time Tree: Only Include Individuals in ABM Map in Seed Tree")
     parser.add_argument('--mutation_rate_loc', required=True, type=float, help="Mutation Rate: Truncated Normal Location (mutations/month)")
     parser.add_argument('--mutation_rate_scale', required=True, type=float, help="Mutation Rate: Truncated Normal Scale (mutations/month)")
+    parser.add_argument('--num_reps_per_score', required=False, type=int, default=5, help="Number of replicates to run per scoring of a parameter set")
+    parser.add_argument('--max_num_threads', required=False, type=int, default=cpu_count(), help="Max number of threads to use")
     parser.add_argument('--gzip_output', action='store_true', help="Gzip Compress Output Files")
     parser.add_argument('--path_abm_hiv_commandline', required=False, type=str, default=DEFAULT_PATH_ABM_HIV_COMMANDLINE, help="Path to abm_hiv-HRSA_SD/abm_hiv_commandline.R")
     parser.add_argument('--path_abm_hiv_modules', required=False, type=str, default=DEFAULT_PATH_ABM_HIV_MODULES, help="Path to abm_hiv-HRSA_SD/modules")
@@ -69,6 +70,10 @@ def parse_args():
 
 # check user args (most will be checked by run_favites_lite.py)
 def check_args(args):
+    # check GNU parallel
+    if not check_output(['parallel', '-h']).decode().strip().startswith('Usage:'):
+        raise RuntimeError("GNU `parallel` does not seem to be installed.\nInstall with e.g.: sudo apt-get install -y parallel")
+
     # check output directory 
     if isdir(args.output) or isfile(args.output):
         raise ValueError("Output directory exists: %s" % args.output)
@@ -77,45 +82,25 @@ def check_args(args):
     if not isfile(args.calibration_csv):
         raise ValueError("Calibration CSV not found: %s" % args.calibration_csv)
 
-# load calibration data from CSV; calibration_data[description] = (value, weight)
-def load_calibration_data(calibration_csv):
-    calibration_data = dict()
-    for row in reader(open(calibration_csv)):
-        d, v, w = [x.strip() for x in row]
-        if d.lower() == 'description':
-            continue # skip header row
-        if d in calibration_data:
-            raise ValueError("Duplicate entry in calibration CSV: %s" % d)
-        calibration_data[d] = (float(v), float(w))
-    return calibration_data
+    # check num reps per score
+    if args.num_reps_per_score < 1:
+        raise ValueError("Number of replicates per scoring must be at least 1: %s" % args.num_reps_per_score)
 
-# load calibration data output by the ABM R code; data[metric][year][subgroup] = count
-def load_abm_calibration_output(abm_calibration_tsv):
-    data = dict()
-    for row in reader(open(abm_calibration_tsv), delimiter='\t'):
-        metric, month, subgroup, stat = [x.strip() for x in row]
-        if metric.lower() == 'metric':
-            continue # skip header row
-        if metric not in data:
-            data[metric] = dict()
-        year = int(month) // 12
-        if year not in data[metric]:
-            data[metric][year] = dict()
-        if subgroup not in data[metric][year]:
-            data[metric][year][subgroup] = 0
-        data[metric][year][subgroup] += int(stat)
-    return data
+    # check max num threads
+    if args.max_num_threads < 1 or args.max_num_threads > cpu_count():
+        raise ValueError("Max number of threads must be at least 1 and at most the total number of CPUs (%d): %s" % (cpu_count(), args.max_num_threads))
 
 # run calibration
 def run_calibration(
-        calibration_data, output, sim_start_time,
+        calibration_csv, output, sim_start_time,
         abm_hiv_params_xlsx, abm_hiv_sd_demographics_csv, abm_hiv_trans_start, abm_hiv_trans_end, abm_hiv_trans_time,
         sample_time_probs_csv, coatran_eff_pop_size, time_tree_seed, time_tree_tmrca, time_tree_only_include_mapped,
         mutation_rate_loc, mutation_rate_scale,
+        num_reps_per_score, max_num_threads,
         path_abm_hiv_commandline=DEFAULT_PATH_ABM_HIV_COMMANDLINE, path_abm_hiv_modules=DEFAULT_PATH_ABM_HIV_MODULES, path_coatran_constant=DEFAULT_PATH_COATRAN_CONSTANT,
     ):
     # prep calibration
-    x0 = [0] # TODO REPLACE WITH ACTUAL STARTING VALUES BASED ON calibration_data
+    x0 = [0] # TODO REPLACE WITH ACTUAL STARTING VALUES BASED ON calibration_csv
     bounds = [
         (-100, 100), # TODO SET BOUNDS FOR x0[0], x0[1], etc.
     ]
@@ -124,26 +109,21 @@ def run_calibration(
     }
     iter_num = 0
     run_favites_lite_path = '%s/%s' % ('/'.join(__file__.replace('/./','/').split('/')[:-1]), 'run_favites_lite.py')
-    abm_xlsx_dir = '%s/abm_xlsx' % output
-    makedirs(abm_xlsx_dir, exist_ok=True)
+    score_simulation_output_path = '%s/%s' % ('/'.join(__file__.replace('/./','/').split('/')[:-1]), 'score_simulation_output.py')
 
     # nested optimization function
     def opt_func(x):
-        # set up FAVITES run
+        # run FAVITES
         nonlocal iter_num; iter_num += 1
         curr_outdir = '%s/favites.output.%s' % (output, str(iter_num).zfill(3))
-        curr_data_xlsx = '%s/abm.data.%s.xlsx' % (abm_xlsx_dir, str(iter_num).zfill(3))
-        wb = load_workbook(abm_hiv_params_xlsx) # load original ABM XLSX
-        wb['High Level Pop + Sim Features']['B4'] = randint(0, MAX_RNG_SEED) # replace ABM seed
-        wb['High Level Pop + Sim Features']['B4'].number_format = '0' # ABM seed is int, not string
-        wb.save(curr_data_xlsx)
-
-        # run FAVITES
+        makedirs(curr_outdir, exist_ok=True)
+        rep_nums = [str(i).zfill(3) for i in range(1, num_reps_per_score+1)]
         run_favites_lite_command = [
+            'parallel', '--jobs', str(max_num_threads),
             run_favites_lite_path,
-            '--output', curr_outdir,
+            '--output', '%s/{}' % curr_outdir,
             '--sim_start_time', str(sim_start_time),
-            '--abm_hiv_params_xlsx', curr_data_xlsx,
+            '--abm_hiv_params_xlsx', abm_hiv_params_xlsx,
             '--abm_hiv_sd_demographics_csv', abm_hiv_sd_demographics_csv,
             '--abm_hiv_trans_start', str(abm_hiv_trans_start),
             '--abm_hiv_trans_end', str(abm_hiv_trans_end),
@@ -160,30 +140,20 @@ def run_calibration(
         ]
         if time_tree_only_include_mapped:
             run_favites_lite_command.append('--time_tree_only_include_mapped')
+        run_favites_lite_command += [':::'] + rep_nums
         print_log("Running FAVITES iteration %d: %s" % (iter_num, ' '.join(run_favites_lite_command)))
         o = check_output(run_favites_lite_command)
 
         # calculate optimization function score TODO REFACTOR INTO OWN FUNCTION
-        abm_calibration_data = load_abm_calibration_output('%s/abm_hiv_calibration_data.tsv' % curr_outdir)
-        score = 0
-        for cal_key, cal_tup in calibration_data.items():
-            sim_val = None; cal_val, cal_w = cal_tup
-            if '_' in cal_key:
-                try:
-                    year, risk = cal_key.split('_')
-                    year = int(year)
-                    risk = risk.replace(' & ', 'and')
-                    if risk.lower() == 'other':
-                        risk = 'other' # 'other' is lowercase in the ABM R code output
-                    sim_val = abm_calibration_data['newinfects_agg'][year-sim_start_time][risk]
-                except:
-                    pass # sim_val will remain None, so ValueError below will be thrown
-            if sim_val is None:
-                raise ValueError("Unknown calibration key: %s" % cal_key)
-            else:
-                score += cal_w * ((sim_val-cal_val)**2)
-        score = score**0.5
-        print_log("FAVITES iteration %d score: %s" % (iter_num, score))
+        score_simulation_output_command = [
+            'parallel', '--jobs', str(max_num_threads),
+            score_simulation_output_path, '%s/{}' % curr_outdir, calibration_csv,
+            '>', '%s/{}/score.txt' % curr_outdir,
+        ] + [':::'] + rep_nums
+        print_log("Calculating optimization function score: %s" % ' '.join(score_simulation_output_command))
+        check_output(score_simulation_output_command)
+        score = mean(float(open(fn).read()) for fn in glob('%s/*/score.txt' % curr_outdir))
+        print_log("FAVITES iteration %d average score: %s" % (iter_num, score))
         return score
 
     # run calibration
@@ -204,14 +174,13 @@ if __name__ == "__main__":
 
     # run calibration
     print_log("===== CALIBRATION =====")
-    print_log("Loading calibration data: %s" % args.calibration_csv)
-    calibration_data = load_calibration_data(args.calibration_csv)
     print_log("Running calibration...")
     run_calibration(
-        calibration_data, args.output, args.sim_start_time,
+        args.calibration_csv, args.output, args.sim_start_time,
         args.abm_hiv_params_xlsx, args.abm_hiv_sd_demographics_csv, args.abm_hiv_trans_start, args.abm_hiv_trans_end, args.abm_hiv_trans_time,
         args.sample_time_probs_csv, args.coatran_eff_pop_size, args.time_tree_seed, args.time_tree_tmrca, args.time_tree_only_include_mapped,
         args.mutation_rate_loc, args.mutation_rate_scale,
+        args.num_reps_per_score, args.max_num_threads,
         args.path_abm_hiv_commandline, args.path_abm_hiv_modules, args.path_coatran_constant,
     )
     pass # TODO CONTINUE HERE
